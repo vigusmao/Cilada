591ffb08c0d0d53ab0d220c56dc7c68509b81d41
vinicius <vinicius@hypermindr.com>
2018-08-28 14:22:22 -0300

    Add default games

--- gridlock.py
+++ gridlock.py
@@ -1,4 +1,10 @@
+# Python 3
+# Jogo Cilada (original Gridlock)
+# https://github.com/vigusmao/Cilada
+
+
 from collections import defaultdict
+from random import randint
 
 shapes_by_piece = {'A': ('O', '+'), \
                    'B': ('Q', '+'), \
@@ -23,8 +29,61 @@ board = [['Q', '+', 'O', 'O'], \
          ['O', 'Q', 'O', 'O'], \
          ['O', 'O', 'Q', 'Q']]
 
+games = { \
+    1: "AABCDDEFGIKN",
+    2: "ABCDDEFFGJKM",
+    3: "ABCCDEFFIJMN",
+    4: "ABDDEEFFIKMN",
+    5: "ABBCDEFFIJKM",
+    6: "ABBCCCDFIJKM",
+    7: "ABBCCCDDEFFKJ",
+    8: "ABBCCCDDHKLN",
+    9: "ABBBCCDDEFFHL",
+    10: "ABBBCCDDEFFLN",
+    11: "ABBBCCCDDFFGI",
+    12: "AABCDEFFGIJM",
+    13: "AABCDDEFGKNM",
+    14: "AABCDEFFGHIJ",
+    15: "AABCDEFFGJMN",
+    16: "AABCCDEFIJKM",
+    17: "AABCCDDEEFFHN",
+    18: "AABCCCDDGHIL",
+    19: "AABBCCDFGIJK",
+    20: "AABBCCDFGJLN",
+    21: "AABBCCCDEFFIJ",
+    22: "AABBCCCDEFFJN",
+    23: "AABBCCCDJKLM",
+    24: "AABBCCCDDEFHK",
+    25: "AABBCCCDEFFIJ",
+    26: "AABBFGIJKMN",
+    27: "AABBBCFFGJMN",
+    28: "AABBBCCCDDFGL",
+    29: "AAABCCDEEFFIJ",
+    30: "AAABCCDEEFFHJ",
+    31: "AAABCDDEEFFGN",
+    32: "AAABCCDDEEFKM",
+    33: "AAABBCEFJKMN",
+    34: "AAABBEFFGJMN",
+    35: "AAABBBCCCDFGH",
+    36: "AAABBBCCEFFIJ",
+    37: "AAABCDEFGIMN",
+    38: "AAABCCDDEEFHL",
+    39: "AAABCCCDGIJL",
+    40: "AAABBCEFIJKM",
+    41: "AAABBCCCDDEEFF",
+    42: "AAABBCCDGKHM",
+    43: "AAAABBCCDEFGI",
+    44: "AAACCCDEIJLN",
+    45: "AAABBCCCIJLN",
+    46: "AAAABCCDEEFHM",
+    47: "AAAABCCDEEFIM",
+    48: "AAAABBBCCCGFJ",
+    49: "AAAABBBCCDEEFF",
+    50: "BBBDDEFFKLMN"}
+
 N_ROWS = len(board)
 N_COLS = len(board[0])
+TOTAL_POSITIONS = N_ROWS * N_COLS
 
 
 matches_by_piece = defaultdict(list)
@@ -102,8 +161,10 @@ def are_positions_available(intended_positions, used_positions):
     return True
 
 def backtrack(missing_pieces, used_positions):
-    if len(missing_pieces) == 0:
+    if len(used_positions) == TOTAL_POSITIONS:
         return True
+    if len(missing_pieces) == 0:
+        return False
     next_piece = missing_pieces.pop()
     for match in matches_by_piece[next_piece]:
         if are_positions_available(match, used_positions):
@@ -117,10 +178,36 @@ def backtrack(missing_pieces, used_positions):
     missing_pieces.append(next_piece)
     return False
          
-def play():
-    game = list(input("Game: ").upper())
+def read_game():
+    print("\n\n-------------\n")
+
+    done = False
+    while not done:
+        sample_game = randint(1, len(games))
+        game = input("Game number (1--%d) or pieces (e.g., %s): "
+                     % (len(games), games[sample_game]))
+        try:
+            game = int(game)
+            if game < 1 or game > len(games):
+                print("Please select a game between 1 and %d." % len(games))
+                continue
+            game = games[game]
+            print("Game pieces: %s" % game)
+            done = True
+        except ValueError:
+            game = list(game.upper())
+            done = True
+            for piece in game:
+                if piece not in shapes_by_piece:
+                    print("Invalid piece: %s. Please try again." % piece)
+                    done = False
+                    break
+            
     if len(game) == 0:
-        return None  # quit
+        return None
+    return list(game)
+    
+def solve(game):
     used_positions = {}
     if backtrack(game, used_positions):
         return used_positions
@@ -132,16 +219,23 @@ def print_solution(solution):
         for col_idx in range(N_COLS):
             print(solution[(row_idx, col_idx)], end=" ")
 
+def list_games():
+    print("\n\nSuggested games:\n")
+    for game, pieces in games.items():
+        print("Game %d: %s" % (game, pieces))
+
 
 prepare()
+list_games()
 
 while True:
-    result = play()
-    if result is None:
+    game = read_game()
+    if game is None:
         break
-    elif result == False:
-        print("This game has no solution.")
-    else:
+    result = solve(game)
+    if result:
         print_solution(result)
-    print("\n\n-------------\n")
+    else:
+        print("This game has no solution.")
+    
     
